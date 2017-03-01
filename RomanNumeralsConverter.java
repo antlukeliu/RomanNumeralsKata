b528ce60322fa8134caac7d319679eb0dd5cf2ad
AntLukeLiu <antmliu@gmail.com>
2017-03-01 13:13:35 -0500

    First commit with first initial running test

---
+++ RomanNumeralsConverter.java
@@ -0,0 +1,15 @@
+//Objective of this is to take in a number and convert it Roman Numeral
+// 1 = "I"
+// 3 = "III"
+// 9 = "IX"
+// 1066 = "MLXVI"
+// 1989 = "MCMLXXXIX"
+
+
+public class RomanNumeralsConverter {
+
+	public String convert(String number) {
+		
+		return "I";
+  }
+}
---
+++ RomanNumeralsConverterTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class RomanNumeralsConverterTest {
+	
+	//Test to see if enter 1 it converts to I
+	@Test
+	public void testforOne() {
+		
+		RomanNumeralsConverter converter = new RomanNumeralsConverter();
+		assertEquals("Test1 didn't work", "I", converter.convert("1"));
+	}
+	
+}
