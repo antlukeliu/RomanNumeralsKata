9c4e9a075c899f0f2653b5eea4999a6a813ad19c
AntLukeLiu <antmliu@gmail.com>
2017-03-01 16:59:27 -0500

    Test3 failed and added thought process on how to complete this problem

--- RomanNumeralsConverter.java
+++ RomanNumeralsConverter.java
@@ -5,7 +5,16 @@
 // 1066 = "MLXVI"
 // 1989 = "MCMLXXXIX"
 
-
+//Thoughts on how to create this converter
+//Plan 1. create an if/else if or case switch for every number
+//Plan 2. Find the length of the number create methods to find each 
+//place(ones, tens, hundreds, thousands) and concat it all together
+//Plan 3. Two arrays i.e. arabicNumArray = [10,9,5,4,1] romanNumArray = ["X", "IX", "V", "IV", "I"]
+//Have a variable holding the romanNumString
+//i.e. the number is 7
+//loop through to find what it is greater than or equal to. It is greater than 5 have V stored in 
+//romanNumString and 2 be the value of your variable number (7-5). Loop again to find 2 >= 1 
+//so "VI" continue until number reaches 0
 
 public class RomanNumeralsConverter {
 
--- RomanNumeralsConverterTest.java
+++ RomanNumeralsConverterTest.java
@@ -1,20 +1,34 @@
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class RomanNumeralsConverterTest {
 	
-	//Test to see if enter 1 it converts to I
+	RomanNumeralsConverter converter;
+
+	@Before
+	public void setUp(){
+		converter = new RomanNumeralsConverter();
+	}
+	
+
+	// Test to see if enter 1 it converts to I
 	@Test
 	public void testforOne() {
-		
-		RomanNumeralsConverter converter = new RomanNumeralsConverter();
+
 		assertEquals("Test1 didn't work", "I", converter.convert("1"));
+	}
+
+	@Test
+	public void testforTwo() {
+
+		assertEquals("Test2 didn't work", "II", converter.convert("2"));
 	}
 	
 	@Test
-	public void testforTwo() {
-		
-		RomanNumeralsConverter converter = new RomanNumeralsConverter();
-		assertEquals("Test2 didn't work", "II", converter.convert("2"));
+	public void testforThree() {
+
+		assertEquals("Test3 didn't work", "III", converter.convert("3"));
 	}
 }
